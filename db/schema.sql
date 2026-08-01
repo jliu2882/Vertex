87b487c0e925fc223e64779f7db2de9317217b48
BuildTools <unconfigured@null.spigotmc.org>
2026-08-01 19:13:06 -0400

    updated task schemas for postgres

--- db/schema.sql
+++ db/schema.sql
@@ -15,6 +15,5 @@ CREATE TABLE IF NOT EXISTS tasks (
     user_id INT NOT NULL REFERENCES users(id) ON DELETE CASCADE,
     title VARCHAR(255) NOT NULL,
     task_description TEXT,
-    completed BOOLEAN DEFAULT FALSE,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
