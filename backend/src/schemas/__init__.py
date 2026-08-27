0491826bd1fa2f6d64ebb2cae85ad36b59398628
Jack Liu <43019654+jliu2882@users.noreply.github.com>
2026-08-26 22:02:04 -0400

    Update __init__.py

--- backend/src/schemas/__init__.py
+++ backend/src/schemas/__init__.py
@@ -1,1 +1,1 @@
-# Not entirely sure, but for some reason, schema doesnt import without __init__.py but services works fine? maybe its not run in docker yet
+# for some reason, schema doesnt import without __init__.py but services works fine? maybe its not run in docker yet
