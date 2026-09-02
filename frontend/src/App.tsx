247b4b55377cbadd3bc9657fd246f66c6445732b
Jack Liu <43019654+jliu2882@users.noreply.github.com>
2026-09-02 19:25:06 -0400

    Fix login route path formatting

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,7 +14,7 @@ function App() {
             <TaskListPage />
         </ProtectedRoute>
       }/>
-      <Route path="/login " element={<LoginPage />} />
+      <Route path="/login" element={<LoginPage />} />
       <Route path="/register" element={<RegisterPage />} />
     </Routes>
   )
